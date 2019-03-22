c304c858de206d2c0b3cf668c1567d3953f59e68
Steven Leong <steveleong8@gmail.com>
2019-03-21 21:12:03 -0600

    Day 11 Done

---
+++ js-challenges/src/projects/11-CustomVideoPlayer/scripts.js
@@ -0,0 +1,65 @@
+// Get Elements
+
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// Build Functions
+
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updatePlayBtn() {
+  const icon = video.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function fill() {
+  const fillPercent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = fillPercent + "%";
+}
+
+function changeSilder() {
+  //   if (this.name === "volume") {
+  //     video.volume = this.value;
+  //   }
+  //   if (this.name === "playbackRate") {
+  //     video.playbackRate = this.value;
+  //   }
+
+  video[this.name] = this.value;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Hook Up Event Listeners
+
+video.addEventListener("click", togglePlay); // play/pause when window is clicked
+video.addEventListener("play", updatePlayBtn); // change from play to paused symbol vv
+video.addEventListener("pause", updatePlayBtn);
+video.addEventListener("timeupdate", fill);
+// setInterval(fill, 50);
+
+toggle.addEventListener("click", togglePlay); // play/pause when button is clicked
+skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", changeSilder));
+ranges.forEach(range => range.addEventListener("mousemove", changeSilder));
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", e => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
